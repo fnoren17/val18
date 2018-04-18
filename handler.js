5767b494d7165cd26daa37ad18edea29b73d5a64
Felix <fnoren17@gmail.com>
2018-04-18 22:56:15 +0200

    Bugg vid click

--- handler.js
+++ handler.js
@@ -1,42 +1,36 @@
-var m_width = $("#map").width(),
-        width = 938,
-        height = 500,
-        country,
-        state;
+var width = 360,
+  height = 600;
+var projection = d3.geo.mercator().scale(1000).translate([-160,1700]);
+var path = d3.geo.path().projection(projection);
+var svg = d3.select("#container").append("svg");
 
-    var projection = d3.geo.mercator()
-        .translate([width / 2.5, height*3.05 ])
-        .scale(900);
-        
-    var path = d3.geo.path()
-        .projection(projection);
-    var svg = d3.select("#map").append("svg")
-        .attr("preserveAspectRatio", "xMidYMid")
-        .attr("viewBox", "0 0 " + width + " " + height)
-        .attr("width", width)
-        .attr("height", width * height / width);
-    svg.append("rect")
-        .attr("class", "background")
-        .attr("width", width)
-        .attr("height", height);
-    var g = svg.append("g");
-
-    d3.json("sweden.topo.json", function(error, us) {
-      g.append("g")
-        .attr("id", "countries")
+d3.json("sweden.topo.json", function(error, data) {
+    if (error) throw error;
+ svg.append("g")
+        .attr("id", "states")
         .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
+        .data(topojson.feature(data, data.objects.states).features)
         .enter()
         .append("path")
         .attr("id", function(d) { return d.id; })
         .attr("d", path)
         .on("click", clicked);
-    });
-function clicked(d){
-    console.log(d.properties.name);
+});
+
+
+function clicked(state){
+    d3.json("kommun.json", function(error, data) {
+    if (error) throw error;
+ svg.append("g")
+        .attr("id", "kommuner")
+        .selectAll("path")
+        .data(topojson.feature(data, data.objects.kommuner).features)
+        .filter(function(d){return d.properties.state == "Stockholm";})
+        .enter()
+        .append("path")
+        .attr("id", function(d) { return d.id; })
+        .attr("d", path);
+        //.on("click", clicked);
+});
+
 }
-    $(window).resize(function() {
-      var w = $("#map").width();
-      svg.attr("width", w);
-      svg.attr("height", w * height / width);
-    });
